0e6cf97185053331ef67b5b5abeceb8a1173fd36
Will Leyton <wleylop22@gmail.com>
2021-03-31 10:26:17 -0500

    Update

--- core/src/test/scala/com/s4n/core/DslSpec.scala
+++ core/src/test/scala/com/s4n/core/DslSpec.scala
@@ -7,75 +7,99 @@ final class DslSpec extends Spec {
 
   "makeCmd(AAAAIAA)" should {
     "Return Init(A(A(A(A(I(A(A(End()))))))))" in {
-      val expected = Init(A(A(A(A(I(A(A(End()))))))))
+      val expected =
+        Init.make(
+          A.make(A.make(A.make(A.make(I.make(A.make(A.make(End.make())))))))
+        )
       makeCmd("AAAAIAA") shouldBe expected
     }
   }
 
   "eval(AADADA)" should {
     "Return (1, 1) South" in {
-      val route = Init(A(A(D(A(D(A(End())))))))
-      val expected = Position(Coordinates(X(1), Y(1)), South)
+      val route =
+        Init.make(A.make(A.make(D.make(A.make(D.make(A.make(End.make())))))))
+      val expected =
+        Position.make(Coordinates.make(X.make(1), Y.make(1)), South)
       eval(route) shouldBe expected
     }
   }
 
   "makeCmd(AADADA)" should {
     "Return Init(A(A(D(A(D(A(End())))))))" in {
-      val expected = Init(A(A(D(A(D(A(End())))))))
+      val expected =
+        Init.make(A.make(A.make(D.make(A.make(D.make(A.make(End.make())))))))
       makeCmd("AADADA") shouldBe expected
     }
   }
 
   "eval(AAAAIAA)" should {
     "Return (-2, 4) East" in {
-      val route = Init(A(A(A(A(I(A(A(End()))))))))
-      val expected = Position(Coordinates(X(-2), Y(4)), East)
+      val route =
+        Init.make(
+          A.make(A.make(A.make(A.make(I.make(A.make(A.make(End.make())))))))
+        )
+      val expected =
+        Position.make(Coordinates.make(X.make(-2), Y.make(4)), East)
       eval(route) shouldBe expected
     }
   }
 
   "makeCmd(DDDAIAD)" should {
     "Return Init(D(D(D(A(I(A(D(End()))))))))" in {
-      val expected = Init(D(D(D(A(I(A(D(End()))))))))
+      val expected =
+        Init.make(
+          D.make(D.make(D.make(A.make(I.make(A.make(D.make(End.make())))))))
+        )
       makeCmd("DDDAIAD") shouldBe expected
     }
   }
 
   "eval(DDDAIAD)" should {
     "Return (-1, -1) East" in {
-      val route = Init(D(D(D(A(I(A(D(End()))))))))
-      val expected = Position(Coordinates(X(-1), Y(-1)), East)
+      val route =
+        Init.make(
+          D.make(D.make(D.make(A.make(I.make(A.make(D.make(End.make())))))))
+        )
+      val expected =
+        Position.make(Coordinates.make(X.make(-1), Y.make(-1)), East)
       eval(route) shouldBe expected
     }
   }
 
   "makeCmd(AAIADAD)" should {
     "Return Init(A(A(I(A(D(A(D(End()))))))))" in {
-      val expected = Init(A(A(I(A(D(A(D(End()))))))))
+      val expected =
+        Init.make(
+          A.make(A.make(I.make(A.make(D.make(A.make(D.make(End.make())))))))
+        )
       makeCmd("AAIADAD") shouldBe expected
     }
   }
 
   "eval(AAIADAD)" should {
     "Return (-1, 3) West" in {
-      val route = Init(A(A(I(A(D(A(D(End()))))))))
-      val expected = Position(Coordinates(X(-1), Y(3)), West)
+      val route =
+        Init.make(
+          A.make(A.make(I.make(A.make(D.make(A.make(D.make(End.make())))))))
+        )
+      val expected =
+        Position.make(Coordinates.make(X.make(-1), Y.make(3)), West)
       eval(route) shouldBe expected
     }
   }
 
   "makeCmd(A)" should {
     "Return Init(A(End()))" in {
-      val expected = Init(A(End()))
+      val expected = Init.make(A.make(End.make()))
       makeCmd("A") shouldBe expected
     }
   }
 
   "eval(A)" should {
     "Return (0, 1) East" in {
-      val route = Init(A(End()))
-      val expected = Position(Coordinates(X(0), Y(1)), North)
+      val route = Init.make(A.make(End.make()))
+      val expected = Position(Coordinates.make(X.make(0), Y.make(1)), North)
       eval(route) shouldBe expected
     }
   }
